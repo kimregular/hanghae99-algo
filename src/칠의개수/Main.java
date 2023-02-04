62e656947100601c1da25d32a504e6722b2b8c8c
regular_kim <regular.jk.kim@gmail.com>
2023-02-04 16:32:53 +0900

    230204 day

---
+++ src/칠의개수/Main.java
@@ -0,0 +1,33 @@
+package 칠의개수;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] array = {10, 29};
+        System.out.println(s.solution(array));
+    }
+}
+
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for (int num : array) {
+            answer += sevenCnt(num);
+        }
+        return answer;
+    }
+
+    public int sevenCnt(int num) {
+        int cnt = 0;
+        int tmp = 0;
+        while (num > 0) {
+            tmp = num % 10;
+            if (tmp == 7) {
+                cnt++;
+            }
+            num /= 10;
+        }
+        return cnt;
+    }
+}
