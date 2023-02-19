2e33e23b5ecf46f970356dc146a087350d338ae3
regular_kim <regular.jk.kim@gmail.com>
2023-02-19 23:21:37 +0900

    230219 evening

--- src/p1158/Main.java
+++ src/p1158/Main.java
@@ -1,62 +1,43 @@
 package p1158;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        BufferedReader rd = new BufferedReader(new FileReader("src/p1158/text.txt"));
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-
         String[] str = rd.readLine().split(" ");
-//        입력값이 띄어쓰기 기준으로 2개
-        int len = Integer.parseInt(str[0]);
-        int num = Integer.parseInt(str[1]);
-        Solution s = new Solution();
-        System.out.println(s.solution(len, num));
-        rd.close();
-    }
-}
+        int N = Integer.parseInt(str[0]);
+        int K = Integer.parseInt(str[1]);
 
-class Solution {
-    public String solution(int len, int num) {
-        StringBuilder answer = new StringBuilder().append("<");
-//        출력 조건에 맞게 세팅
-        Queue<Integer> myQ = new LinkedList<>();
-//        큐는 추상 클래스, 따라서 구현은 연결리스트로 진행
-        for (int i = 1; i < len+1; i++) {
-            myQ.add(i);
-//            받은 수만큼 큐에 저장
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(i + 1);
         }
-        int cnt = 1;
-//        1, 2, 3 했을 때 연산이 되야 하므로 1부터 시작
-        while (!myQ.isEmpty()) {
-            // 큐에서 계속해서 값을 뺄거다.
-            myQ.add(myQ.poll());
-//            맨 앞 값 뺀 후 맨 뒤에 추가
-            cnt++;
-//            카운트 더함
-            if (cnt == num) {
-//                지정된 수와 카운트가 같다면
-                answer.append(myQ.poll()).append(", ");
-//                큐 맨 앞값을 뺴고, 뺀 값을 정답에 추가
-                cnt = 1;
-//                카운트 초기화
-            }
+
+        int[] arr = new int[N];
+        int idx = 0;
+        int remove = 0;
+        for (int i = 0; i < N; i++) {
+            remove = (remove + K - 1) % list.size();
+            arr[idx++] = list.get(remove);
+            list.remove(remove);
         }
-        answer.delete(answer.length() - 2, answer.length());
-//        1,2,3,4,5, 처럼 마지막에 쉼표가 붙으므로 마지막 쉼표 빼주기
-//        delete(start, end)
-        answer.append(">");
-//      출력조건에 맞게 세팅
 
+        StringBuilder result = new StringBuilder().append("<");
+        for (int i = 0; i < N; i++) {
+            result.append(arr[i]);
+            if (i != N - 1)
+                result.append(", ");
+            else
+                result.append(">");
+        }
 
-
-        return answer.toString();
-//        스트링 빌더를 스트링으로 변환
+        System.out.println(result);
     }
+
 }
