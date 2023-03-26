f7bbdbcd8b781976526b35285d6cfbfe1712afba
regular_kim <regular.jk.kim@gmail.com>
2023-03-26 18:36:56 +0900

    230326 evening

---
+++ src/p4344/Main.java
@@ -0,0 +1,57 @@
+package p4344;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
+public class Main {
+    static int num;
+
+    static int mean;
+
+    static double ratio;
+
+
+    static int getMean(String[] strings) {
+        int total = 0;
+
+        for (int i = 1; i < strings.length; i++) {
+            total += Integer.parseInt(strings[i]);
+        }
+
+        return total / Integer.parseInt(strings[0]);
+    }
+
+    static double getRatio(String[] strings, int mean) {
+        int cnt = 0;
+
+        for (int i = 1; i < strings.length; i++) {
+            if (Integer.parseInt(strings[i]) > mean) {
+                cnt++;
+            }
+        }
+
+        return cnt / Double.parseDouble(strings[0]) * 100;
+    }
+
+    static String getThree(double number) {
+        DecimalFormat df = new DecimalFormat("0.000");
+        return df.format(number);
+    }
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new FileReader("src/p4344/text.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        num = Integer.parseInt(br.readLine());
+        for (int i = 0; i < num; i++) {
+            String[] st = br.readLine().split(" ");
+            mean = getMean(st);
+            ratio = getRatio(st, mean);
+            System.out.println(getThree(ratio)+"%");
+        }
+
+        br.close();
+    }
+}
