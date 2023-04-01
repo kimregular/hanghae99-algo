150359258413b8909fef4f7881a0dfeeda455d62
regular_kim <regular.jk.kim@gmail.com>
2023-04-01 18:43:18 +0900

    230401 evening

---
+++ src/p2558/Main.java
@@ -0,0 +1,22 @@
+package p2558;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int num1;
+    static int num2;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        num1 = Integer.parseInt(br.readLine());
+        num2 = Integer.parseInt(br.readLine());
+
+        System.out.println(num1 + num2);
+
+        br.close();
+    }
+}
