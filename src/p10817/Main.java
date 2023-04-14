9544b214dcd56ccfdedcdfca2b9a904ba4d10da6
regular_kim <regular.jk.kim@gmail.com>
2023-04-14 19:12:07 +0900

    230414 evening

---
+++ src/p10817/Main.java
@@ -0,0 +1,27 @@
+package p10817;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int arr[] = new int[3];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+
+
+        br.close();
+    }
+}
