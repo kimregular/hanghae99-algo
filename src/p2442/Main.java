e8999f7f0a4c89798012d4a52ffc16a8c4f25d55
regular_kim <regular.jk.kim@gmail.com>
2023-05-02 02:11:10 +0900

    230502 morning

---
+++ src/p2442/Main.java
@@ -0,0 +1,31 @@
+package p2442;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int num;
+
+    static void getStars(int num) {
+        String star = "*";
+        String empty = " ";
+        for (int i = 0; i < num; i++) {
+            String stars = star.repeat(2*i + 1);
+            String empties = empty.repeat(num - i - 1);
+            System.out.println(empties + stars);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        num = Integer.parseInt(br.readLine());
+
+        getStars(num);
+
+        br.close();
+    }
+}
