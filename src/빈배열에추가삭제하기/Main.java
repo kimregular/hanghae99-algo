bbabe07c3196d5a35a992762cfff05b5961c392c
regular_kim <regular.jk.kim@gmail.com>
2023-07-05 20:51:37 +0900

    230705 evening

---
+++ src/빈배열에추가삭제하기/Main.java
@@ -0,0 +1,21 @@
+package 빈배열에추가삭제하기;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution {
+
+    public int[] solution(int[] arr, boolean[] flag) {
+        List<Integer> answer = new ArrayList<>();
+
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
