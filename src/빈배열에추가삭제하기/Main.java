1275a03ed9221df54c64801d6906e9c10ac111f5
kimregular <regular.jk.kim@gmail.com>
2023-07-05 22:30:20 +0900

    230705 evening

--- src/빈배열에추가삭제하기/Main.java
+++ src/빈배열에추가삭제하기/Main.java
@@ -1,10 +1,17 @@
 package 빈배열에추가삭제하기;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        int[] arr = {3, 2, 4, 1, 3};
+        boolean[] flag = {true, false, true, false, false};
+
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(arr, flag)));
 
     }
 }
@@ -15,6 +22,19 @@ class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
         List<Integer> answer = new ArrayList<>();
 
+        int idx = 0;
+        for (int i = 0; i < flag.length; i++) {
+            if (flag[idx]) {
+                for (int j = 0; j < arr[idx] * 2; j++) {
+                    answer.add(arr[idx]);
+                }
+            } else {
+                for (int j = 0; j < arr[idx]; j++) {
+                    answer.remove(answer.size() - 1);
+                }
+            }
+            idx++;
+        }
 
         return answer.stream().mapToInt(i -> i).toArray();
     }
