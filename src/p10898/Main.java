325a4559b180781d79782e4dae6b4feee2476d8a
regular_kim <regular.jk.kim@gmail.com>
2023-07-28 20:20:15 +0900

    230728 evening

---
+++ src/p10898/Main.java
@@ -0,0 +1,27 @@
+package p10898;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] arr = new int[10001];
+
+            for (int i = 0; i < n; i++) {
+                arr[Integer.parseInt(br.readLine())] += 1;
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] > 0) {
+                    for (int j = 0; j < arr[i]; j++) {
+                        System.out.println(i);
+                    }
+                }
+            }
+        }
+    }
+}
