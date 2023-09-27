3e11031ef567ea36f2516d6206033edfc73fdf16
regular_kim <regular.jk.kim@gmail.com>
2023-09-27 17:15:49 +0900

    BJ : https://www.acmicpc.net/problem/16953

---
+++ src/p16953/Main.java
@@ -0,0 +1,57 @@
+package p16953;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            long num = Long.parseLong(st.nextToken());
+            long target = Long.parseLong(st.nextToken());
+
+            Solution s = new Solution(num, target);
+            System.out.println(s.BFS());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Solution {
+
+    long num;
+    long target;
+
+    public Solution(long num, long target) {
+        this.num = num;
+        this.target = target;
+    }
+
+    public long BFS() {
+        int cnt = 0;
+        Queue<Long> q = new LinkedList<>();
+        q.offer(num);
+
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for(int i = 0; i < len; i++){
+
+                long tmp = q.poll();
+                if (tmp == target) return cnt+1;
+                else {
+                    long num1 = tmp * 2;
+                    long num2 = tmp * 10 + 1;
+
+                    if(num1 <= target) q.offer(num1);
+                    if(num2 <= target) q.offer(num2);
+                }
+            }
+            cnt++;
+        }
+
+        return -1;
+    }
+}
