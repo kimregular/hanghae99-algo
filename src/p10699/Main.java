3f5edf2d1aa27659c6ab1bb7684abd023116d675
kimregular <regular.jk.kim@gmail.com>
2023-09-29 15:48:21 +0900

    BJ : https://www.acmicpc.net/problem/10699

---
+++ src/p10699/Main.java
@@ -0,0 +1,10 @@
+package p10699;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args){
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
