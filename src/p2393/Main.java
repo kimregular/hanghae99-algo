c7409ff0107dfd5e6fdfb8992542c2b69e8a2508
kimregular <regular.jk.kim@gmail.com>
2023-10-23 22:09:49 +0900

    BJ : https://www.acmicpc.net/problem/2393

---
+++ src/p2393/Main.java
@@ -0,0 +1,19 @@
+package p2393;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+    }
+}
