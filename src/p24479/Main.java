97118ed22e0c3634f711d6a925b2099d5c82dc18
kimregular <regular.jk.kim@gmail.com>
2023-11-07 21:41:30 +0900

    BJ : https://www.acmicpc.net/problem/24479

---
+++ src/p24479/Main.java
@@ -0,0 +1,73 @@
+package p24479;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int numOfNodes = Integer.parseInt(st.nextToken());
+            GraphExplorer ge = new GraphExplorer(numOfNodes);
+
+            int numOfEdges = Integer.parseInt(st.nextToken());
+            int startNode = Integer.parseInt(st.nextToken());
+
+            for (int i = 0; i < numOfEdges; i++) {
+                st = new StringTokenizer(br.readLine(), " ");
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                ge.connect(x, y);
+            }
+
+            for (ArrayList<Integer> ints : ge.graph) {
+                Collections.sort(ints);
+            }
+
+            System.out.println(ge.explore(startNode));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class GraphExplorer {
+
+    ArrayList<ArrayList<Integer>> graph;
+    int[] isVisited;
+    int order = 1;
+
+    public GraphExplorer(int numOfNodes) {
+        this.graph = new ArrayList<>();
+        for (int i = 0; i < numOfNodes + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+        this.isVisited = new int[numOfNodes + 1];
+    }
+
+    public void connect(int x, int y) {
+        graph.get(x).add(y);
+        graph.get(y).add(x);
+    }
+
+    public String explore(int startNode) {
+        StringBuilder result = new StringBuilder();
+        DFS(startNode);
+        for (int i = 1; i < isVisited.length; i++) {
+            result.append(isVisited[i]).append("\n");
+        }
+
+        return result.toString();
+    }
+
+    private void DFS(int startNode) {
+        isVisited[startNode] = order++;
+        for (Integer nextNode : graph.get(startNode)) {
+            if(isVisited[nextNode] != 0) continue;
+            DFS(nextNode);
+        }
+    }
+}
