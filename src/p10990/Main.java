dd1ce3f6eefd84b18bb7960a4da80a5c823e4a0c
regular_kim <regular.jk.kim@gmail.com>
2024-01-05 19:11:56 +0900

    BJ : https://www.acmicpc.net/problem/10990

---
+++ src/p10990/Main.java
@@ -0,0 +1,46 @@
+package p10990;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int num = Integer.parseInt(br.readLine());
+
+            Solution s = new Solution(num);
+            System.out.println(s.solution());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Solution {
+
+    int num;
+
+    public Solution(int num) {
+        this.num = num;
+    }
+
+    public String solution() {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < this.num; i++) {
+            for (int j = 0; j < this.num - i - 1; j++) {
+                result.append(" ");
+            }
+            result.append("*");
+            for (int x = 0; x < 2 * i - 1; x++) {
+                result.append(" ");
+            }
+            if (i != 0) {
+                result.append("*").append("\n");
+            } else {
+                result.append("\n");
+            }
+        }
+        return result.toString();
+    }
+}
+
