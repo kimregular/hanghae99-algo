fb708055ebe0034763462941d9bf65d5218d3d50
kimregular <regular.jk.kim@gmail.com>
2024-01-07 17:09:11 +0900

    BJ : https://www.acmicpc.net/problem/5339

---
+++ src/p5339/Main.java
@@ -0,0 +1,16 @@
+package p5339;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
