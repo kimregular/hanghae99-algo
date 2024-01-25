8c6452f3538b6823c2744061e9148d98691b5af7
kimregular <regular.jk.kim@gmail.com>
2024-01-25 22:15:57 +0900

    BJ : https://www.acmicpc.net/problem/1935

---
+++ src/p1935/Main.java
@@ -0,0 +1,62 @@
+package p1935;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int len = Integer.parseInt(br.readLine());
+
+            char[] postfix = br.readLine().toCharArray();
+
+            double[] nums = new double[len];
+            for (int i = 0; i < len; i++) {
+                nums[i] = Double.parseDouble(br.readLine());
+            }
+
+            System.out.printf("%.2f", s.solution(postfix, nums));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class Solution {
+
+    public double solution(char[] postfix, double[] nums) {
+        Stack<Double> numStck = new Stack<>();
+        Stack<Character> operStck = new Stack<>();
+
+        for (char x : postfix) {
+            if ('A' <= x && x <= 'Z') {
+                numStck.push(nums[x - 'A']);
+            } else {
+                double num2 = numStck.pop();
+                double num1 = numStck.pop();
+                numStck.push(operation(num1, num2, x));
+            }
+        }
+        return numStck.pop();
+    }
+
+    private double operation(double num1, double num2, char oper) {
+        switch (oper) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+        }
+        return 0;
+    }
+
+}
