b6355ccee68f495c55986ae2a3bfc27c87d3b423
kimregular <regular.jk.kim@gmail.com>
2024-02-18 20:12:30 +0900

    BJ https://www.acmicpc.net/problem/10189

---
+++ src/p10189/Main.java
@@ -0,0 +1,14 @@
+package p10189;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String answer = "#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #";
+
+        System.out.println(answer);
+    }
+}
