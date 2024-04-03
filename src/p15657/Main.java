2df4b817fb88b2612d0afae395c1e8be8422d3dc
kimregular <regular.jk.kim@gmail.com>
2024-04-03 20:08:33 +0900

    BJ https://www.acmicpc.net/problem/15657

---
+++ src/p15657/Main.java
@@ -0,0 +1,95 @@
+package p15657;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Input ip = getInput(br);
+            Solution s = new Solution();
+            System.out.println(s.solution(ip.group, ip.select));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Input getInput(BufferedReader br) throws IOException {
+        String[] tokens = br.readLine().split(" ");
+
+        int len = Integer.parseInt(tokens[0]);
+        int select = Integer.parseInt(tokens[1]);
+
+        int[] group = new int[len];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < group.length; i++) {
+            group[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(group);
+
+        return new Input(group, select);
+    }
+
+    private static class Input {
+        int[] group;
+        int select;
+
+        public Input(int[] group, int select) {
+            this.group = group;
+            this.select = select;
+        }
+    }
+
+}
+
+class Solution {
+
+    public String solution(int[] group, int select) {
+        CombinationResolver cr = new CombinationResolver(group, select);
+        return cr.getResult();
+    }
+
+    private class CombinationResolver {
+
+        int[] group;
+        int select;
+        int[] combinated;
+        StringBuilder result = new StringBuilder();
+
+        public CombinationResolver(int[] group, int select) {
+            this.group = group;
+            this.select = select;
+            this.combinated = new int[select];
+        }
+
+        public String getResult() {
+            repeatedCombinate(0, 0);
+            return this.result.toString();
+        }
+
+        private void repeatedCombinate(int cnt, int start) {
+            if (cnt == this.select) {
+                saveCombinated(this.combinated);
+                return;
+            }
+
+            for (int i = start; i < this.group.length; i++) {
+                this.combinated[cnt] = this.group[i];
+                repeatedCombinate(cnt + 1, i);
+            }
+        }
+
+        private void saveCombinated(int[] combinated) {
+            for (int i : combinated) {
+                this.result.append(i).append(" ");
+            }
+            this.result.append("\n");
+        }
+    }
+
+}
