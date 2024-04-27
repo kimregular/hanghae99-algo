81997156b2993dc4f40cd55b8d43082802b60f75
kimregular <regular.jk.kim@gmail.com>
2024-04-27 17:48:47 +0900

    BJ : https://www.acmicpc.net/problem/15680

---
+++ src/p15680/Main.java
@@ -0,0 +1,29 @@
+package p15680;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Solution s = new Solution();
+            System.out.println(s.solution(getInput(br)));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int getInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
+
+class Solution {
+
+    public String solution(int num) {
+        if(num == 0) return "YONSEI";
+        return "Leading the Way to the Future";
+    }
+}
