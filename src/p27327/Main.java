9d858d5a7f7b7a3eccb296383f5ca551c40966e8
kimregular <regular.jk.kim@gmail.com>
2024-05-17 22:38:27 +0900

    BJ : https://www.acmicpc.net/problem/27327

---
+++ src/p27327/Main.java
@@ -0,0 +1,28 @@
+package p27327;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Solution s = new Solution();
+            System.out.println(s.solution(getInput(br)));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int getInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
+
+class Solution {
+
+    public int solution(int time) {
+        return time * 24;
+    }
+}
