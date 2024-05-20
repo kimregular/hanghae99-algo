09f78e8957f51d8e941008b5ba35627bcb61d3dd
kimregular <regular.jk.kim@gmail.com>
2024-05-20 19:57:53 +0900

    BJ : https://www.acmicpc.net/problem/10987

---
+++ src/p10987/Main.java
@@ -0,0 +1,36 @@
+package p10987;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Solution s = new Solution();
+            System.out.println(s.solution(getInput(br)));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String getInput(BufferedReader br) throws IOException {
+        return br.readLine();
+    }
+}
+
+class Solution {
+
+    public int solution(String str){
+        int cnt = 0;
+        for(char c :str.toCharArray()){
+            if(isVowel(c)) cnt++;
+        }
+        return cnt;
+    }
+
+    private boolean isVowel(char c){
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
