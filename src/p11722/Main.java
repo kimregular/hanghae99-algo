4fe8fff9be1a86aea316ece7fe10b6dfb5372900
kimregular <regular.jk.kim@gmail.com>
2024-08-02 12:33:54 +0900

    BJ : https://www.acmicpc.net/problem/11722

--- src/p11722/Main.java
+++ src/p11722/Main.java
@@ -1,52 +1,48 @@
 package p11722;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int len = Integer.parseInt(br.readLine());
-            int[] arr = new int[len];
 
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            Solution s = new Solution();
+            System.out.println(s.solution(getInput(br)));
 
-            for (int i = 0; i < len; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
-            }
-
-            Solution s = new Solution(arr);
-            System.out.println(s.solution());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static int[] getInput(BufferedReader br) throws IOException {
+        int len = Integer.parseInt(br.readLine());
+        int[] result = new int[len];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Integer.parseInt(st.nextToken());
+        }
+        return result;
     }
 }
 
 class Solution {
 
-    int[] arr;
-
-    public Solution(int[] arr) {
-        this.arr = arr;
-    }
-
-    public int solution() {
-        int[] dp = new int[this.arr.length];
-
-        for (int i = 0; i < this.arr.length; i++) {
-            dp[i] = 1;
-
-            for (int j = 0; j < i; j++) {
-                if(this.arr[i] < this.arr[j] && dp[j] > dp[i] - 1) dp[i]++;
-            }
-        }
-
+    public int solution(int[] nums) {
+        if(nums.length == 1) return 1;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
         int result = 0;
-        for (int d : dp) {
-            if(result < d) result = d;
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] < nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            result = Math.max(result, dp[i]);
         }
-//        System.out.println(Arrays.toString(dp));
         return result;
     }
 }
