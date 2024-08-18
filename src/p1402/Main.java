15db8507c1c49aa3ef4f5461ba3e78dfd3f2bf12
kimregular <regular.jk.kim@gmail.com>
2024-08-18 16:58:54 +0900

    BJ : https://www.acmicpc.net/problem/1402

---
+++ src/p1402/Main.java
@@ -0,0 +1,33 @@
+package p1402;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Solution s = new Solution();
+            System.out.println(s.solution(getInput(br)));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static int getInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
+
+class Solution {
+
+    StringBuilder answer = new StringBuilder();
+
+    public String solution(int repeatNum) {
+        while(repeatNum --> 0) answer.append("yes").append("\n");
+        return answer.toString();
+    }
+}
