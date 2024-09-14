c56212a6e41124e146bc5ef159389e74b58125a2
regular_kim <regular.jk.kim@gmail.com>
2024-09-14 18:14:23 +0900

    BJ : https://www.acmicpc.net/problem/10170

---
+++ src/p10170/Main.java
@@ -0,0 +1,24 @@
+package p10170;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String target = "NFC West       W   L  T\n" +
+                "-----------------------\n" +
+                "Seattle        13  3  0\n" +
+                "San Francisco  12  4  0\n" +
+                "Arizona        10  6  0\n" +
+                "St. Louis      7   9  0\n" +
+                "\n" +
+                "NFC North      W   L  T\n" +
+                "-----------------------\n" +
+                "Green Bay      8   7  1\n" +
+                "Chicago        8   8  0\n" +
+                "Detroit        7   9  0\n" +
+                "Minnesota      5  10  1";
+        System.out.println(target);
+    }
+
+
+
+}
