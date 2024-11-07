cf404ee4896376919ee1f30ddc73668cc53100a4
kimregular <regular.jk.kim@gmail.com>
2024-11-08 08:32:25 +0900

    BJ : https://www.acmicpc.net/problem/1931

--- src/p1931/Main.java
+++ src/p1931/Main.java
@@ -1,69 +1,58 @@
 package p1931;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
+        new Main().run();
+    }
 
-        ArrayList<Schedule> arr = new ArrayList<>();
+    private void run() {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int numOfTemporarySchedules = Integer.parseInt(br.readLine());
-            for (int i = 0; i < numOfTemporarySchedules; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-                int startTime = Integer.parseInt(st.nextToken());
-                int endTime = Integer.parseInt(st.nextToken());
-                arr.add(new Schedule(startTime, endTime));
-            }
 
-            arr.sort(Comparator.naturalOrder());
-            ScheduleMaker sm = new ScheduleMaker(arr);
-            System.out.println(sm.getNumOfSchedules());
+            Solution s = new Solution();
+            System.out.println(s.solution(readInput(br)));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    private int[][] readInput(BufferedReader br) throws IOException {
+        int len = Integer.parseInt(br.readLine());
+        int[][] result = new int[len][2];
+
+        for (int i = 0; i < len; i++) {
+            String[] tokens = br.readLine().split(" ");
+            int start = Integer.parseInt(tokens[0]);
+            int end = Integer.parseInt(tokens[1]);
+            result[i][0] = start;
+            result[i][1] = end;
+        }
+        return result;
+    }
 }
 
-class ScheduleMaker {
+class Solution {
 
-    ArrayList<Schedule> scheduleList;
+    public int solution(int[][] schedules) {
+        Arrays.sort(schedules, (a, b) -> {
+            if(a[1] == b[1]) return Integer.compare(a[0], b[0]);
+            return a[1] - b[1];
+        });
 
-    public ScheduleMaker(ArrayList<Schedule> scheduleList) {
-        this.scheduleList = scheduleList;
-    }
-
-    public int getNumOfSchedules() {
-        int numOfSchedule = 0;
-        int nextScheduleTime = 0;
-
-        for (Schedule nextSchedule : scheduleList) {
-            if (nextSchedule.startTime >= nextScheduleTime) {
-                numOfSchedule++;
-                nextScheduleTime = nextSchedule.endTime;
+        int answer = 0;
+        int lastMeetingEnd = 0;
+        for (int[] schedule : schedules) {
+            if (schedule[0] >= lastMeetingEnd) {
+                lastMeetingEnd = schedule[1];
+                answer++;
             }
         }
-
-        return numOfSchedule;
-    }
-}
-
-class Schedule implements Comparable<Schedule> {
-    int startTime;
-    int endTime;
-
-    public Schedule(int startTime, int endTime) {
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    @Override
-    public int compareTo(Schedule o) {
-        if(this.endTime > o.endTime) return 1;
-        if (this.endTime == o.endTime) {
-            if(this.startTime > o.startTime) return 1;
-            else return -1;
-        }
-        return -1;
+        return answer;
     }
 }
