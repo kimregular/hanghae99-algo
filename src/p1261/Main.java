9b1a04cc24ea6cd32ac9b52057e443ba3b2d9bd3
Regular Kim <82751941+kimregular@users.noreply.github.com>
2024-11-15 12:14:43 +0900

    BJ : https://www.acmicpc.net/problem/1261

--- src/p1261/Main.java
+++ src/p1261/Main.java
@@ -3,10 +3,8 @@ package p1261;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class Main {
 
