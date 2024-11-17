b174e6234c6efa0ff1c78c6d463130ab5c393bbf
kimregular <regular.jk.kim@gmail.com>
2024-11-17 19:16:38 +0900

    BJ : https://www.acmicpc.net/problem/30214

---
+++ src/p30214/Main.java
@@ -0,0 +1,39 @@
+package p30214;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Solution s = new Solution();
+            System.out.println(s.solution(readInput(br)));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int[] readInput(BufferedReader br) throws IOException {
+        return Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+}
+
+class Solution {
+
+    public String solution(int[] nums) {
+        return isEasy(nums) ? "E" : "H";
+    }
+
+    private boolean isEasy(int[] nums) {
+        return nums[0] >= nums[1] - nums[0];
+    }
+}
