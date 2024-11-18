7ed52ca0965c9ceaa70e72c5ab6faade2de9810d
kimregular <regular.jk.kim@gmail.com>
2024-11-18 16:13:25 +0900

    BJ : https://www.acmicpc.net/problem/11659

--- src/p11659/Main.java
+++ src/p11659/Main.java
@@ -1,35 +1,85 @@
 package p11659;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int numOfDigits = Integer.parseInt(st.nextToken());
-            int TEST_CASE = Integer.parseInt(st.nextToken());
+        new Main().run();
+    }
 
-            st = new StringTokenizer(br.readLine(), " ");
-            int[] arr = new int[numOfDigits + 1];
-            arr[0] = 0;
-            for (int i = 1; i < numOfDigits + 1; i++) {
-                arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
-            }
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-            for (int i = 0; i < TEST_CASE; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
+            Input ip = readInput(br);
+            Solution s = new Solution();
+            System.out.println(s.solution(ip.arr, ip.ranges));
 
-                int result = arr[end] - arr[start - 1];
-
-                bw.write(String.valueOf(result));
-                bw.write("\n");
-            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Input readInput(BufferedReader br) throws IOException {
+        String[] tokens = br.readLine().split(" ");
+        int len = Integer.parseInt(tokens[0]);
+        int rangeLen = Integer.parseInt(tokens[1]);
+
+        int[] arr = new int[len];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < len; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] ranges = new int[rangeLen][2];
+        for (int i = 0; i < rangeLen; i++) {
+            tokens = br.readLine().split(" ");
+            int from = Integer.parseInt(tokens[0]);
+            int to = Integer.parseInt(tokens[1]);
+            ranges[i][0] = from;
+            ranges[i][1] = to;
+        }
+
+        return new Input(arr, ranges);
+    }
+
+    private static class Input {
+
+        private final int[] arr;
+        private final int[][] ranges;
+
+        public Input(int[] arr, int[][] ranges) {
+            this.arr = arr;
+            this.ranges = ranges;
+        }
+    }
+}
+
+class Solution {
+
+    public String solution(int[] arr, int[][] ranges) {
+        int[] prefixSums = getPrefixSums(arr);
+
+        StringBuilder answer = new StringBuilder();
+
+        for (int[] range : ranges) {
+            int from = range[0] - 1;
+            int to = range[1];
+            answer.append(prefixSums[to] - prefixSums[from]).append("\n");
+        }
+        return answer.toString();
+    }
+
+    private int[] getPrefixSums(int[] arr) {
+        int len = arr.length;
+        int[] prefixSums = new int[len + 1];
+
+        for (int i = 1; i < len + 1; i++) {
+            prefixSums[i] = prefixSums[i - 1] + arr[i - 1];
+        }
+        return prefixSums;
     }
 }
