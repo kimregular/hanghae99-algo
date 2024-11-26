fb5c15ecc343bf0b956b724bc6b719003aeb0fc5
kimregular <regular.jk.kim@gmail.com>
2024-11-26 19:46:08 +0900

    BJ : https://www.acmicpc.net/problem/21598

---
+++ src/p21598/Main.java
@@ -0,0 +1,28 @@
+package p21598;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            int n = readInput(br);
+            for (int i = 0; i < n; i++) {
+                System.out.println("SciComLove");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int readInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
