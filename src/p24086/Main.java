8c76ea8776441fa33d4f11794011221994e2d3fd
kimregular <regular.jk.kim@gmail.com>
2024-11-27 17:12:45 +0900

    BJ : https://www.acmicpc.net/problem/24086

---
+++ src/p24086/Main.java
@@ -0,0 +1,31 @@
+package p24086;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            int[] input = readInput(br);
+            System.out.println(input[1] - input[0]);
+            return;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int[] readInput(BufferedReader br) throws IOException {
+        int[] result = new int[2];
+        result[0] = Integer.parseInt(br.readLine());
+        result[1] = Integer.parseInt(br.readLine());
+        return result;
+    }
+}
