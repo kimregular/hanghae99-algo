2f0e3a0703ea6b649ce9fdecb29e35bb2a91cfae
kimregular <regular.jk.kim@gmail.com>
2024-11-30 08:06:44 +0900

    BJ : https://www.acmicpc.net/problem/28444

---
+++ src/p28444/Main.java
@@ -0,0 +1,28 @@
+package p28444;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            int[] input = readInput(br);
+
+            System.out.println(input[0] * input[1] - input[2] * input[3] * input[4]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int[] readInput(BufferedReader br) throws IOException {
+        return Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+}
