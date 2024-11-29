66600a736d939510039d90f1f6fccc2cd512b71d
kimregular <regular.jk.kim@gmail.com>
2024-11-29 14:31:18 +0900

    BJ : https://www.acmicpc.net/problem/23234

---
+++ src/p23234/Main.java
@@ -0,0 +1,12 @@
+package p23234;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        System.out.println("The world says hello!");
+    }
+}
