bbafe33d95b38ece152df1460cba5db5c64b39e4
kimregular <regular.jk.kim@gmail.com>
2024-12-01 13:51:34 +0900

    BJ : https://www.acmicpc.net/problem/32154

---
+++ src/p32154/Main.java
@@ -0,0 +1,38 @@
+package p32154;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            String[][] board = {{"11", "A B C D E F G H J L M"},
+                    {"9", "A C E F G H I L M"},
+                    {"9", "A C E F G H I L M"},
+                    {"9", "A B C E F G H L M"},
+                    {"8", "A C E F G H L M"},
+                    {"8", "A C E F G H L M"},
+                    {"8", "A C E F G H L M"},
+                    {"8", "A C E F G H L M"},
+                    {"8", "A C E F G H L M"},
+                    {"8", "A B C F G H L M"}};
+
+            int i = readInput(br) - 1;
+            System.out.println(board[i][0]);
+            System.out.println(board[i][1]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int readInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
