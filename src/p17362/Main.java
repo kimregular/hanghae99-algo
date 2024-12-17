7743f1b58b73c3c28f6206e55ee0c42ac54ac0ac
kimregular <regular.jk.kim@gmail.com>
2024-12-17 10:26:14 +0900

    BJ : https://www.acmicpc.net/problem/17362

---
+++ src/p17362/Main.java
@@ -0,0 +1,35 @@
+package p17362;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            Solution s = new Solution();
+            System.out.println(s.solution(readInput(br)));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int readInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
+
+class Solution {
+
+    public int solution(int target) {
+        int[] fingers = {1, 2, 3, 4, 5, 4, 3, 2};
+        return fingers[(target - 1) % 8];
+    }
+}
