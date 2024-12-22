2505f4f322a49052c32fdcfcaf2c64a43f8a0254
kimregular <regular.jk.kim@gmail.com>
2024-12-22 17:29:27 +0900

    BJ : https://www.acmicpc.net/problem/32951

---
+++ src/p32951/Main.java
@@ -0,0 +1,26 @@
+package p32951;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println(readInput(br) - 2024);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int readInput(BufferedReader br) throws IOException {
+        return Integer.parseInt(br.readLine());
+    }
+}
